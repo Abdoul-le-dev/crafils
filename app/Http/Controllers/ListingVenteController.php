5741384a515b78b60b1ec5d0f40708f42bad3007
Abdoul-le-dev <abdoulledev@gmail.com>
2024-06-26 15:45:59 +0000

    cra

---
+++ app/Http/Controllers/ListingVenteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreListingVenteRequest;
+use App\Http\Requests\UpdateListingVenteRequest;
+use App\Models\ListingVente;
+
+class ListingVenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreListingVenteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ListingVente $listingVente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ListingVente $listingVente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateListingVenteRequest $request, ListingVente $listingVente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ListingVente $listingVente)
+    {
+        //
+    }
+}
